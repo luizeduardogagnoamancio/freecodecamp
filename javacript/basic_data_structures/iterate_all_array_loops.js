15e83c36b3d34332e5ed4d996652a51f6ecf67b4
LuizAmancio <luizgagnoamancio@gmail.com>
2022-01-13 12:35:25 -0300

    .

---
+++ javacript/basic_data_structures/iterate_all_array_loops.js
@@ -0,0 +1,13 @@
+function filteredArray(arr, elem) {
+    let newArr = [];
+    // Only change code below this line
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].indexOf(elem) != -1) newArr = arr.slice(i, 1)
+    }
+    // Only change code above this line
+    return newArr;
+  }
+  
+console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
+  
+console.log(filteredArray([[10, 8, 3], [14, 6, 23], [3, 18, 6]], 18));
